e0ccfd016c130c9ff6a837d8404a571fd81ebfbf
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-05-28 18:04:16 -0700

    setOwner

--- contracts/interfaces/IVAULT.sol
+++ contracts/interfaces/IVAULT.sol
@@ -12,6 +12,10 @@ interface IVAULT {
     /// @notice Returns the address of the vault owner
     function owner() external view returns (address);
 
+    /// @notice Sets the owner of the vault (only callable by protocol during position creation)
+    /// @param _owner Address of the new owner
+    function setOwner(address _owner) external;
+
     /// @notice Transfers tokens from the vault to the owner
     /// @param token Address of the token to transfer
     /// @param amount Amount to transfer
